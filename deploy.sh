da7b6c48a40534a3621e17534cab2ef632857983
joaofcosta <dinojoaocosta@gmail.com>
2020-04-03 12:40:10 +0100

    Update deploy script to make tags work
    Since I have a `tags` folder in my global .gitignore file the `tags`
    folder in the `public` folder of the website was not being committed to
    the repository, thus the tags were not working on the live version.
    
    This commit updates the deploy script to forcefully (-f) add all the
    files inside the public folder, which will add all nested folders inside
    `public`, making the tags system work again.

--- deploy.sh
+++ deploy.sh
@@ -12,7 +12,10 @@ printf "\033[0;32mDeploying updates to GitHub...\033[0m\n"
 cd public
 
 # Add changes to git.
-git add .
+# I need to use the -f flag here otherwise the folders inside the 'tags' folder
+# won't be added because I have the 'tags' folder in the global .gitignore file
+# because of Exuberant CTags...
+git add . -f
 
 # Commit changes.
 msg="rebuilding site $(date)"
